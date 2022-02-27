2de3c35387b1c5c75fda187780e3a7f74db7c4db
Tran Duy Tung <dtroyal1102@gmail.com>
2022-02-27 12:22:47 +0700

    Material manager

---
+++ src/com/company/CrispyFlour.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class CrispyFlour extends Material{
+    private int quantity;
+
+    public CrispyFlour() {
+    }
+
+    public CrispyFlour(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public CrispyFlour(String id, String name, LocalDate manufacturingDate, int cost, int quantity) {
+        super(id, name, manufacturingDate, cost);
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public double getAmount() {
+       return quantity * getCost();
+    }
+
+    @Override
+    public LocalDate getExpiryDate() {
+        return getManufacturingDate().plusYears(1);
+    }
+
+    @Override
+    public double getRealMoney() {
+        LocalDate today = LocalDate.now();
+        if (today.isBefore(getExpiryDate())&&
+                (today.isAfter(getExpiryDate().minusMonths(2)))){
+            System.out.println("--> Giá bột đang giảm 40%: ");
+
+            return getAmount()*0.6;
+        }
+        else if(today.isBefore(getExpiryDate().minusMonths(2)) &&
+                (today.isAfter(getExpiryDate().minusMonths(4)))){
+            System.out.println("--> Giá bột đang giảm 20%: ");
+
+            return getAmount()*0.8;
+        }
+        else if(today.isBefore(getExpiryDate().minusMonths(4))&&
+                (today.isAfter(getManufacturingDate()))){
+            System.out.println("--> Giá bột đang giảm 5%: ");
+
+            return getAmount()*0.95;
+        }
+        else {
+            System.out.println("!@##$$ Bột đã hết hạn: ");
+            return -1;
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Bột chiên giòn: " +
+                super.toString() +
+                ",Số lượng = " + quantity +
+                ",Tổng tiền = " + getRealMoney();
+
+    }
+}
---
+++ src/com/company/Meat.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Meat extends Material {
+    private double weight;
+
+    public Meat() {
+    }
+
+    public Meat(double weight) {
+        this.weight = weight;
+    }
+
+    public Meat(String id, String name, LocalDate manufacturingDate, int cost, double weight) {
+        super(id, name, manufacturingDate, cost);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public double getAmount() {
+        return weight * getCost();
+    }
+
+    @Override
+    public LocalDate getExpiryDate() {
+        return getManufacturingDate().plusDays(7);
+    }
+
+    @Override
+    public double getRealMoney() {
+        LocalDate today = LocalDate.now();
+        if (today.isBefore(getExpiryDate()) &&
+                (today.isAfter(getExpiryDate().minusDays(5)))){
+            System.out.println("--> Giá thịt đang giảm 30%: ");
+            return getAmount() * 0.7;
+        } else if (today.isBefore(getExpiryDate().minusDays(5)) &&
+                today.isAfter(getManufacturingDate())){
+            System.out.println("--> Giá thịt đang giảm 10%: ");
+            return getAmount() * 0.9;
+        } else {
+            System.out.println("!@#$ Thịt đã hết hạn: ");
+            return -1;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Thịt: " +
+                super.toString() +
+                ",Trọng lượng = " + weight +
+                ",Tổng tiền = " + getRealMoney();
+    }
+}
